6b192812d6d6722053b7247787f3ffebd77c88c5
cqkenx <cqkenx@126.com>
2020-12-02 15:24:05 +0800

    clear~
    !

--- 208MySQL迁移Oracle/generated/2018-07-25_14-41-18/.wrapper.sql
+++
@@ -1,8 +0,0 @@
--- Password for 'term_cloud_208' user
-define term_cloud_208_password=term_cloud_208
-
--- Password for 'Emulation' user
-define Emulation_password=Emulation
-
-@@"/Users/shanyingqi/DNAX/208MySQL迁移Oracle/generated/2018-07-25_14-41-18/master.sql"
-
--- 208MySQL迁移Oracle/generated/2018-07-25_14-41-18/dropallschemas.sql
+++
@@ -1,6 +0,0 @@
-PROMPT Dropping user term_cloud_208
-DROP USER term_cloud_208 CASCADE;
-
-PROMPT Dropping user Emulation
-DROP USER Emulation CASCADE;
-
